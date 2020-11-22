d9cd96b238966963392284942576beedc88b5908
Otaviano <otaviano.almeida@hotmail.com>
2020-11-22 15:57:56 -0300

    SellerDao's findById implementation and db.properties fixed

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,24 @@
 package model.dao.impl;
 
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
+
+   private Connection connection;
+
+   public SellerDaoJDBC(Connection connection){
+       this.connection = connection;
+   }
+
     @Override
     public void insert(Seller seller) {
 
@@ -23,7 +36,29 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try{
+            st = connection.prepareStatement("SELECT seller.*,department.Name as DepName "
+                    + "FROM seller INNER JOIN department "
+                    + "ON seller.DepartmentId = department.Id "
+                    + "WHERE seller.Id = ?");
+
+            st.setInt(1, id);
+            rs = st.executeQuery();
+            if (rs.next()){
+                return new Seller(rs.getInt("Id"),
+                        rs.getString("Name"),
+                        rs.getString("Email"),
+                        rs.getDate("birthDate"),
+                        rs.getDouble("baseSalary"),
+                        new Department(rs.getInt("departmentId"), rs.getString("depName")));
+            }
+            return null;
+        }
+        catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
     }
 
     @Override
